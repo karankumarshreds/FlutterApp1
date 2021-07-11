d63e48c230ffb1b131c6c24c74049c64cf160ce8
karankumarshreds <naileditnerd@gmail.com>
2021-07-11 22:39:16 +0530

    add background color

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
     MaterialApp(
-        home: Center(
-            child: Text('Hello World!')
-        )
-    )
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I am rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
 );
